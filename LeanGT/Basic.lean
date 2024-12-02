35db9d1225ebf6987ec8bc5ac85615663c91dd48
Li Xuanji <xuanji@gmail.com>
2024-12-01 20:06:45 -0800

    docs

--- LeanGT/Basic.lean
+++ LeanGT/Basic.lean
@@ -105,36 +105,6 @@ theorem mem_A_iff (g : DihedralGroup 4) : g ∈ A ↔ g = r 0 ∨ g = r 1 ∨ g 
 
 def C_A : Subgroup (DihedralGroup 4) := Subgroup.centralizer A
 
-example (G : Type) [Group G] (H : Subgroup G) : Subgroup.centralizer H ≤ Subgroup.normalizer H := by {
-  intros g g_centralizes_h
-  have ginv_centralizes_h : g⁻¹ ∈ Subgroup.centralizer H := by {
-    exact?
-  }
-
-  have conj_by_ginv' (h : G) : h ∈ H → g⁻¹ * h * g ∈ H := by {
-    intro hh
-    specialize ginv_centralizes_h h
-    rw [←ginv_centralizes_h]
-    simp
-    exact hh
-    exact hh
-  }
-  rw [Subgroup.mem_centralizer_iff] at g_centralizes_h
-  rw [Subgroup.mem_normalizer_iff]
-  intros h
-  constructor
-  intros hh
-  have rw1 := g_centralizes_h h hh
-  rw [← rw1]
-  group
-  exact hh
-  intros h'
-  have spec := conj_by_ginv' (g * h * g⁻¹) h'
-  group at spec
-  exact spec
-}
-
-
 
 
 -- example 2 on page 50
