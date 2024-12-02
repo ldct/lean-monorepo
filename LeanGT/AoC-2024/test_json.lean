5c559723e82708ef82e150d1ffd1838ceb2a948b
Li Xuanji <xuanji@gmail.com>
2024-12-01 23:50:33 -0800

    aoc

---
+++ LeanGT/AoC-2024/test_json.lean
@@ -0,0 +1,21 @@
+import Lean
+import Lean.Data.Json.Basic
+import Lean.Data.Json.Parser
+import Lean.Data.Json.Printer
+
+open Lean Json ToJson FromJson
+
+structure Wrapper: Type where
+  data: Array Int
+deriving ToJson, FromJson, Inhabited, Repr
+
+def get_ledger_from_json_string (s: String): Except String Wrapper := do
+  let j : Json <- Json.parse s
+  let ledger : Wrapper <- fromJson? j
+  return ledger
+
+def test_str := "{
+  \"data\": [1, 2, 3]
+}
+"
+#eval (get_ledger_from_json_string test_str)
