a90ee95d9569d58a13f7e15c9870bc65f53fab0b
Li Xuanji <xuanji@gmail.com>
2025-03-02 22:54:18 -0500

    docs

--- LeanGT/Explain.lean
+++ LeanGT/Explain.lean
@@ -1,48 +1,10 @@
 /-
-Copyright (c) 2024 Adam Topaz. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adam Topaz
+texify is a tactic that displays the goal in latex form.
+
+Adapted from explanation widget by Adam Topaz.
 -/
 import Mathlib
 import ProofWidgets.Component.HtmlDisplay
-
-/-!
-
-# Explanation Widgets
-
-This file defines some simple widgets wrapped in a tactic, term and command elaborator
-that display nicely rendered markdown in the infoview.
-
-Example tactic usage:
-```
-example : True := by
-  explain "This is the first step."
-  explain "This is the last step." in
-    exact trivial
-```
-Placing the cursor on each line results will render the explanation in the infoview.
-
-Example term usage:
-```
-example : Nat := explain "This is zero" in
-  0
-```
-Placing the cursor on the term will render the explanation in the infoview.
-
-
-Example command usage:
-```
-#explain "This is an explanation."
-```
-This will render the provided text in the infoview.
-
-
-# Implementation
-
-This code uses `MarkdownDisplay` from `ProofWidgets`, and thus supports
-markdown and latex via mathjax.
-
--/
 
 open Lean Elab ProofWidgets ProofWidgets.Jsx
 
@@ -54,7 +16,7 @@ def displayMarkdown (md : String) (stx : Syntax) : CoreM Unit := do
     (return json% { html: $(← Server.RpcEncodable.rpcEncode html) })
     stx
 
-syntax (name := explainTacStx) "texify" : tactic
+syntax (name := texifyTacticSyntax) "texify" : tactic
 
 
 set_option linter.unusedTactic false
@@ -62,6 +24,7 @@ set_option linter.unusedVariables false
 
 namespace Lean.Expr
 
+/-- A brute force pretty printer for expressions. -/
 partial def brute_force_pp (expr : Expr) : String := match expr with
   | .bvar idx => s!"x_{idx}"
   | .fvar id => "fvar"
@@ -76,6 +39,7 @@ partial def brute_force_pp (expr : Expr) : String := match expr with
   | .lit (.strVal s) => s!"\"{s}\""
   | _ => "???"
 
+/-- Try to pretty print an expression in latex form. -/
 partial def expr_to_latex (expr : Expr) (ctx : LocalContext) : String := Id.run do
 
   if expr.isFVar then
@@ -91,48 +55,65 @@ partial def expr_to_latex (expr : Expr) (ctx : LocalContext) : String := Id.run 
   if (← pure (expr.isAppOfArity ``HAdd.hAdd 6)) then
     match (← pure (getAppArgs expr)) with
     | #[a, b, c, d, e, f] => return s!"{expr_to_latex e ctx} + {expr_to_latex f ctx}"
-    | _ => return brute_force_pp expr
+    | _ =>
+      dbg_trace f!"unexpected arity for HAdd.hAdd"
+      return brute_force_pp expr
 
   if (← pure (expr.isAppOfArity ``HDiv.hDiv 6)) then
     match (← pure (getAppArgs expr)) with
     | #[a, b, c, d, e, f] => return s!"\\frac \{ {expr_to_latex e ctx} } \{{expr_to_latex f ctx}}"
-    | _ => return brute_force_pp expr
+    | _ =>
+      dbg_trace f!"unexpected arity for HDiv.hDiv"
+      return brute_force_pp expr
 
   if (← pure (expr.isAppOfArity ``LT.lt 4)) then
     match (← pure (getAppArgs expr)) with
     | #[a, b, c, d] => return s!"{expr_to_latex c ctx} < {expr_to_latex d ctx}"
-    | _ => return brute_force_pp expr
+    | _ =>
+      dbg_trace f!"unexpected arity for LT.lt"
+      return brute_force_pp expr
 
   if (← pure (expr.isAppOfArity ``LE.le 4)) then
     match (← pure (getAppArgs expr)) with
     | #[a, b, c, d] => return s!"{expr_to_latex c ctx} \\leq {expr_to_latex d ctx}"
-    | _ => return brute_force_pp expr
+    | _ =>
+      dbg_trace f!"unexpected arity for LE.le"
+      return brute_force_pp expr
 
   if (← pure (expr.isAppOfArity ``HSub.hSub 6)) then
     match (← pure (getAppArgs expr)) with
     | #[a, b, c, d, e, f] => return s!"{expr_to_latex e ctx} - {expr_to_latex f ctx}"
-    | _ => return brute_force_pp expr
+    | _ =>
+      dbg_trace f!"unexpected arity for HSub.hSub"
+      return brute_force_pp expr
 
   if (← pure (expr.isAppOfArity ``HMul.hMul 6)) then
     match (← pure (getAppArgs expr)) with
     | #[a, b, c, d, e, f] => return s!"{expr_to_latex e ctx} {expr_to_latex f ctx}"
-    | _ => return brute_force_pp expr
+    | _ =>
+      dbg_trace f!"unexpected arity for HMul.hMul"
+      return brute_force_pp expr
 
   if (← pure (expr.isAppOfArity ``HPow.hPow 6)) then
     match (← pure (getAppArgs expr)) with
     | #[a, b, c, d, e, f] => return s!"({expr_to_latex e ctx})^{expr_to_latex f ctx}"
-    | _ => return brute_force_pp expr
+    | _ =>
+      dbg_trace f!"unexpected arity for HPow.hPow"
+      return brute_force_pp expr
 
   if (← pure (expr.isAppOfArity ``Eq 3)) then
     match (← pure (getAppArgs expr)) with
     | #[a, b, c] => return s!"{expr_to_latex b ctx} = {expr_to_latex c ctx}"
-    | _ => return brute_force_pp expr
+    | _ =>
+      dbg_trace f!"unexpected arity for Eq"
+      return brute_force_pp expr
 
   return brute_force_pp expr
 
 open Tactic in
+
 /-- A tactic that adds an explanation widget in markdown form. -/
-@[tactic explainTacStx]
+@[tactic texifyTacticSyntax]
 def elabExplainTac : Tactic := fun stx =>
   match stx with
   | `(tactic|texify%$tk) => do
@@ -145,7 +126,7 @@ def elabExplainTac : Tactic := fun stx =>
   | _ => throwUnsupportedSyntax
 
 
-example (x y : ℝ): 0 < (x/y) := by
+example (x y : ℝ): 0 < (x*(x+y)) := by
   texify
   sorry
 
