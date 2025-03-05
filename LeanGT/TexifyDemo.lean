4fe1de00b322535d0b701c4688482b0270f71ea5
Li Xuanji <xuanji@gmail.com>
2025-03-05 00:49:20 -0500

    reorder

--- LeanGT/TexifyDemo.lean
+++ LeanGT/TexifyDemo.lean
@@ -13,14 +13,8 @@ theorem motzkin (x y : ℝ) : 0 ≤ x^4 * y^2 + x^2 * y^4  - 3 * x^2 * y^2 + 1 :
   texify
   intentional_sorry
 
-theorem nesbitt (a b c : ℝ) (ha : 0 < a) (hb : 0 < b) (hc : 0 < c)
-  : (3:ℝ) / 2 ≤ a / (b + c) + b / (a + c) + c / (a + b) := by
-  texify
-  intentional_sorry
-
-theorem imo2008_p2a (x y z : ℝ) (h : x * y * z = 1) (hx : x ≠ 1) (hy : y ≠ 1) (hz : z ≠ 1) :
-  -- IMO 2008 P2, from compfiles
-    1 ≤ x ^ 2 / (x - 1) ^ 2 + y ^ 2 / (y - 1) ^ 2 + z ^ 2 / (z - 1) ^ 2 := by
+theorem mathlinks (a b c : ℝ) (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) (h : a*b*c = 1)
+: 3 ≤ Real.sqrt ((a + b) / (a + 1)) + Real.sqrt ((b + c) / (b + 1)) + Real.sqrt ((c + a) / (c + 1)) := by
   texify
   intentional_sorry
 
@@ -46,6 +40,17 @@ theorem imo1966_p4 (n : ℕ) (x : ℝ)
 
 -- More inequalities
 
+theorem nesbitt (a b c : ℝ) (ha : 0 < a) (hb : 0 < b) (hc : 0 < c)
+  : (3:ℝ) / 2 ≤ a / (b + c) + b / (a + c) + c / (a + b) := by
+  texify
+  intentional_sorry
+
+theorem imo2008_p2a (x y z : ℝ) (h : x * y * z = 1) (hx : x ≠ 1) (hy : y ≠ 1) (hz : z ≠ 1) :
+  -- IMO 2008 P2, from compfiles
+    1 ≤ x ^ 2 / (x - 1) ^ 2 + y ^ 2 / (y - 1) ^ 2 + z ^ 2 / (z - 1) ^ 2 := by
+  texify
+  intentional_sorry
+
 theorem imosl1998SL (x y z : ℝ) (hx : 0 < x) (hy : 0 < y) (hz : 0 < z) (h : x*y*z = 1)
   : (3:ℝ) / 4 ≤ x^3 / ((1 + y) * (1 + z)) + y^3 / ((1 + z) * (1 + x)) + z^3 / ((1 + x) * (1 + y)):= by
   texify
@@ -53,11 +58,6 @@ theorem imosl1998SL (x y z : ℝ) (hx : 0 < x) (hy : 0 < y) (hz : 0 < z) (h : x*
 
 theorem usamo1998 (a b c : ℝ) (ha : 0 < a) (hb : 0 < b) (hc : 0 < c)
   : 1 / (a*b*c) ≤ 1 / (a^3 + b^3 + a*b*c) + 1 / (b^3 + c^3 + a*b*c) + 1 / (c^3 + a^3 + a*b*c) := by
-  texify
-  intentional_sorry
-
-theorem mathlinks (a b c : ℝ) (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) (h : a*b*c = 1)
-: 3 ≤ Real.sqrt ((a + b) / (a + 1)) + Real.sqrt ((b + c) / (b + 1)) + Real.sqrt ((c + a) / (c + 1)) := by
   texify
   intentional_sorry
 
