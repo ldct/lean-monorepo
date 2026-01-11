9d8f0c69e5b6f3ebe76397b8a919957ac26821d7
Li Xuanji <xuanji@duolingo.com>
2026-01-11 21:12:57 +0800

    cleanup

--- v24/playground/Playground/Basic.lean
+++ v24/playground/Playground/Basic.lean
@@ -1,6 +1,10 @@
 import Mathlib
 
 example (n : Nat) : (n * (n + 1)) % 2 = 0 := by
+<<<<<<< HEAD
   exact Nat.mod_eq_zero_of_dvd (Nat.even_mul_succ_self n).two_dvd
+=======
+  sorry
+>>>>>>> aed8f9f (cleanup)
 
 example : 1 + 1 = 2 := by norm_num
