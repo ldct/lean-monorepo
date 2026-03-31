25284a2d8d3369d087d534ce39306519cbe2887f
Xuanji Li <xuanji@gmail.com>
2026-03-31 19:58:26 -0400

    explorations

--- v28/playground/Playground/Determinant2.lean
+++ v28/playground/Playground/Determinant2.lean
@@ -58,6 +58,64 @@ private def M₄ : Matrix (Fin 8) (Fin 8) ℤ :=
       0,  0,  0,  0,  0, -1,  2, -1;
       0,  0,  0,  0,  0,  0, -1,  2]
 
+open Lean Meta in
+unsafe def evalNatExpr (e : Expr) : MetaM (Option Nat) := do
+  let ty ← inferType e
+  let .const ``Nat _ := ty | return none
+  let val ← Lean.Meta.evalExpr Nat (mkConst ``Nat) e
+  return some val
+
+def e : Lean.Expr := .lit (.natVal 3)
+
+open Qq in
+def e' : Lean.Expr := q(3)
+#eval evalNatExpr e'
+
+open Qq in
+def e'' : Lean.Expr := q(![1, 2, 3, 4, 5, 6, 7, 8])
+
+
+
+open Qq in
+def e''' : Lean.Expr := q(!![1, 2, 3, 4, 5, 6, 7, 8; 9, 10, 11, 12, 13, 14, 15, 16])
+
+open Lean Meta Qq in
+def myFun (e : Expr) : MetaM (Option (Fin 8 → ℤ)) := fun e => do
+  return (some ![1, 2, 3, 4, 5, 6, 7, 8])
+
+#eval myFun e'''
+
+#check Matrix.vecCons
+
+variable {a b c d e f g h : ℤ}
+
+#check Matrix.cons_val
+example : ![a] 0 = a := by dsimp only [Matrix.cons_val]
+
+open Lean Meta Qq in
+simproc_decl my_simproc (M₄ _) := fun e => do
+  let row ← unsafe evalExpr (Fin 8 → ℤ) ( q(Fin 8 → ℤ)) e
+  let r0 := row ⟨0, by decide⟩
+  let r1 := row ⟨1, by decide⟩
+  let r2 := row ⟨2, by decide⟩
+  let r3 := row ⟨3, by decide⟩
+  let r4 := row ⟨4, by decide⟩
+  let r5 := row ⟨5, by decide⟩
+  let r6 := row ⟨6, by decide⟩
+  let r7 := row ⟨7, by decide⟩
+  logInfo m!"my_simproc row = ![{r0}, {r1}, {r2}, {r3}, {r4}, {r5}, {r6}, {r7}]"
+  let z0 : Q(ℤ) := mkIntLit r0
+  let z1 : Q(ℤ) := mkIntLit r1
+  let z2 : Q(ℤ) := mkIntLit r2
+  let z3 : Q(ℤ) := mkIntLit r3
+  let z4 : Q(ℤ) := mkIntLit r4
+  let z5 : Q(ℤ) := mkIntLit r5
+  let z6 : Q(ℤ) := mkIntLit r6
+  let z7 : Q(ℤ) := mkIntLit r7
+  let result : Q(Fin 8 → ℤ) := q(![$z0, $z1, $z2, $z3, $z4, $z5, $z6, $z7])
+  let proof ← mkDecideProof (← mkEq e result)
+  return .done { expr := result, proof? := some proof }
+
 private def M₅ : Matrix (Fin 8) (Fin 8) ℤ :=
   !![ 2,  0, -1,  0,  0,  0,  0,  0;
       0,  2,  0, -1,  0,  0,  0,  0;
@@ -89,13 +147,6 @@ private def M₇ : Matrix (Fin 8) (Fin 8) ℤ :=
       0,  0,  0,  0,  0,  0,  0,  1]
 
 open Lean Meta in
-unsafe def evalNatExpr (e : Expr) : MetaM (Option Nat) := do
-  let ty ← inferType e
-  let .const ``Nat _ := ty | return none
-  let val ← Lean.Meta.evalExpr Nat (mkConst ``Nat) e
-  return some val
-
-open Lean Meta in
 simproc_decl count_ofNat (Nat.count _ _) := fun e => do
   let some val ← unsafe evalNatExpr e | return .continue
   let result := mkNatLit val
@@ -105,23 +156,8 @@ simproc_decl count_ofNat (Nat.count _ _) := fun e => do
 lemma count_201_true_eq_201 : Nat.count (fun _ ↦ True) 201 = 201 := by
   simp only [count_ofNat]
 
-open Lean Meta in
-unsafe def evalIntExpr (e : Expr) : MetaM (Option Int) := do
-  let ty ← inferType e
-  let .const ``Int _ := ty | return none
-  let val ← Lean.Meta.evalExpr Int (mkConst ``Int) e
-  return some val
 
-open Lean Meta in
-simproc_decl eval_M₄_entry (M₄ _ _) := fun e => do
-  let some val ← unsafe evalIntExpr e | return .continue
-  let result := Lean.mkIntLit val
-  let proof ← mkDecideProof (← mkEq e result)
-  return .done { expr := result, proof? := some proof }
 
-lemma m45_helper : M₄ 5 = ![0, 0, 0, 0, -1, 2, -1, 0] := by
-  funext i
-  fin_cases i <;> simp [eval_M₄_entry]
 
 theorem E₈_det : E₈.det = 1 := by
   apply my_helper 2 0 (2:ℤ) (1:ℤ)
