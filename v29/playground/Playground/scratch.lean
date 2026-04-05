2b69080c9af1e8cfc771f1939cb6c9af1c48e8c8
Xuanji Li <xuanji@gmail.com>
2026-04-05 19:50:35 -0400

    create v29

---
+++ v29/playground/Playground/scratch.lean
@@ -0,0 +1,13 @@
+import Mathlib
+
+example (a b : ℕ) : a + b = b + a := by
+  rw [add_comm]
+
+example (a b : ℕ) : a + b = b + a := by
+  rw [add_comm]
+
+example (a b : ℕ) : a + b = b + a := by
+  rw [add_comm]
+
+example (a b : ℕ) : a + b = b + a := by
+  rw [add_comm]
