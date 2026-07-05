75af4cdb6219bf4171cc7a8edb464cad31c95595
Xuanji Li <xuanji@gmail.com>
2026-07-05 17:46:40 -0400

    basic extensions

---
+++ v31/playground/Playground/GroupTheory/Extensions/C2_C3.lean
@@ -0,0 +1,86 @@
+import Mathlib
+
+abbrev C2 := Multiplicative (ZMod 2)
+abbrev C3 := Multiplicative (ZMod 3)
+abbrev C6 := Multiplicative (ZMod 6)
+
+/-!
+C6 is an extension of C2 by C3
+
+Key definitions
+- `C2_extension`: the extension of C2
+-/
+
+def f : C3 →* C6 := {
+  toFun x := match (x.toAdd) with
+  | 0 => (.ofAdd 0)
+  | 1 => (.ofAdd 2)
+  | 2 => (.ofAdd 4)
+  map_one' := by decide
+  map_mul' x y := by
+    fin_cases x
+    <;> fin_cases y
+    <;> decide
+}
+
+lemma f_injective : Function.Injective f := by
+  intro a b h
+  fin_cases a <;> fin_cases b
+  <;> simp +decide at *
+
+def g : C6 →* C2 := {
+  toFun x := match (x.toAdd) with
+  | 0 => (.ofAdd 0)
+  | 1 => (.ofAdd 1)
+  | 2 => (.ofAdd 0)
+  | 3 => (.ofAdd 1)
+  | 4 => (.ofAdd 0)
+  | 5 => (.ofAdd 1)
+  map_one' := by decide
+  map_mul' x y := by
+    simp only [toAdd_mul, ofAdd_zero]
+    fin_cases x
+    <;> fin_cases y
+    <;> simp +decide
+}
+
+lemma g_surjective : Function.Surjective g := by
+  intro y
+  fin_cases y <;> decide
+
+def im_f : Subgroup C6 := {
+  carrier := { .ofAdd 0, .ofAdd 2, .ofAdd 4 }
+  mul_mem' := by decide
+  one_mem' := by decide
+  inv_mem' := by decide
+}
+
+lemma f_im_eq_im_f : f.range = im_f := by
+  ext x
+  simp [f, im_f]
+  constructor
+  · rintro ⟨ a, ha ⟩
+    fin_cases a <;> simp_all
+  · rintro h
+    obtain rfl | rfl | rfl := h <;> decide
+
+lemma ker_g_eq_im_f : g.ker = im_f := by
+  ext x
+  simp [g, im_f]
+  constructor
+  · intro h
+    fin_cases x <;> simp_all
+    · left ; rfl
+    · right ; left ; rfl
+    · right ; right ; rfl
+  · rintro h
+    obtain rfl | rfl | rfl := h <;> decide
+
+def C3_extension : GroupExtension C3 C6 C2 := {
+  inl := f
+  rightHom := g
+  inl_injective := f_injective
+  range_inl_eq_ker_rightHom := by
+    grind [f_im_eq_im_f, ker_g_eq_im_f]
+  rightHom_surjective := g_surjective
+}
---
+++ v31/playground/Playground/GroupTheory/Extensions/C3_C2.lean
@@ -0,0 +1,91 @@
+import Mathlib
+
+abbrev C2 := Multiplicative (ZMod 2)
+abbrev C3 := Multiplicative (ZMod 3)
+abbrev C6 := Multiplicative (ZMod 6)
+
+/-!
+C6 is an extension of C3 by C2
+
+Key definitions
+- `C3_extension`: the extension of C3
+-/
+
+def f' : C2 →* C6 where
+  toFun x := Multiplicative.ofAdd (3 * (x.toAdd.val : ZMod 6))
+  map_one' := by decide
+  map_mul' := by decide --why does it work here
+
+def f : C2 →* C6 := {
+  toFun x := match (x.toAdd) with
+  | 0 => (.ofAdd 0)
+  | 1 => (.ofAdd 3)
+  map_one' := by decide
+  map_mul' x y := by
+    fin_cases x
+    <;> fin_cases y
+    <;> decide
+}
+
+lemma f_injective : Function.Injective f := by
+  intro a b h
+  fin_cases a <;> fin_cases b
+  <;> simp +decide at *
+
+def g : C6 →* C3 := {
+  toFun x := match (x.toAdd) with
+  | 0 => (.ofAdd 0)
+  | 1 => (.ofAdd 1)
+  | 2 => (.ofAdd 2)
+  | 3 => (.ofAdd 0)
+  | 4 => (.ofAdd 1)
+  | 5 => (.ofAdd 2)
+  map_one' := by decide
+  map_mul' x y := by
+    simp only [toAdd_mul, ofAdd_zero]
+    fin_cases x
+    <;> fin_cases y
+    <;> simp +decide
+}
+
+lemma g_surjective : Function.Surjective g := by
+  intro y
+  fin_cases y <;> decide
+
+def im_f : Subgroup C6 := {
+  carrier := { .ofAdd 0, .ofAdd 3 }
+  mul_mem' := by decide
+  one_mem' := by decide
+  inv_mem' := by decide
+}
+
+lemma f_im_eq_im_f : f.range = im_f := by
+  ext x
+  simp [f, im_f]
+  constructor
+  · rintro ⟨ a, ha ⟩
+    fin_cases a <;> simp_all
+  · rintro h
+    obtain rfl | rfl := h <;> decide
+
+lemma ker_g_eq_im_f : g.ker = im_f := by
+  ext x
+  simp [g, im_f]
+  constructor
+  · intro h
+    fin_cases x <;> simp_all
+    · left ; rfl
+    · grind
+    · right ; rfl
+    · grind
+  · rintro h
+    obtain rfl | rfl := h <;> decide
+
+def C3_extension : GroupExtension C2 C6 C3 := {
+  inl := f
+  rightHom := g
+  inl_injective := f_injective
+  range_inl_eq_ker_rightHom := by
+    grind [f_im_eq_im_f, ker_g_eq_im_f]
+  rightHom_surjective := g_surjective
+}
